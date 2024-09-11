dc31a7db35923e8fe5f6997fc7d53f8562c0f0a6
jiwoo05117 <051124smwu@sookmyung.ac.kr>
2024-09-11 14:36:28 +0900

    week 02

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b; //variables
+	
+	a = 10;
+	b = 20;
+	
+	printf("Hellow, Sookmyung Electronics!/n");
+	print("%d + %d = %d /n", a, b, a+b);
+	return 0;
+}
