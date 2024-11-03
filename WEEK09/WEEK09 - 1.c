a3e2c334018be54d313c8698315d455abd37ac5d
jiwoo05117 <051124smwu@sookmyung.ac.kr>
2024-11-03 20:25:55 +0900

    WEEK09-1

---
+++ WEEK09/WEEK09 - 1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main(void)
+{
+  int flag = 1;
+  while(flag != 0)
+  {
+             int y;
+             y = 3;
+             flag = 0;
+             }
+  y=4;//오류발생 
+}*/
+
+int main(void)
+{
+  int flag = 1;
+  int y;
+  
+  while(flag != 0){
+             y = 3;
+             flag = 0;
+  }
+  
+  y = 4;
+  printf("y = %d\n", y);
+  
+  return 0; 
+}
