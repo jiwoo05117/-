d59d1d39905362008936b6451f722adaed30616c
jiwoo05117 <051124smwu@sookmyung.ac.kr>
2024-11-24 08:05:17 +0900

    week11(3)

---
+++ week11/week11(3).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+void main(void) {
+    char str[] = "The worst things to eat before you sleep"; 
+    char dst[100];
+
+    
+    strcpy(dst, str);  
+
+   
+    printf("copied string: %s", dst);  
+}
