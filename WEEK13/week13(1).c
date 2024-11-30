87ace5421504645b77f1b89d78773cdabc05c6b0
jiwoo05117 <051124smwu@sookmyung.ac.kr>
2024-11-30 14:51:36 +0900

    week13(1)

--- WEEK13/week13(1).c
+++ WEEK13/week13(1).c
@@ -4,22 +4,27 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 struct student {
-    int id;        
-    char name[50]; 
-    float gpa;     
+    int id;            
+    char name[50];     
+    float grade;       
 };
 
 int main(void) {
+    
     struct student s1;
+    s1.id = 20231234;              
+    strcpy(s1.name, "Minho");      
+    s1.grade = 3.75;                
 
-    s1.id = 20231234;                  
-    strcpy(s1.name, "Kim Minho");      
-    s1.gpa = 3.75;                   
+    
+    s1.id = 24;                     
+    strcpy(s1.name, "Eunju");      
+    s1.grade = 4.2;                 
 
-   
-    printf("학번: %d\n", s1.id);
-    printf("이름: %s\n", s1.name);
-    printf("학점: %.2f\n", s1.gpa);
+    
+    printf("ID: %d\n", s1.id);
+    printf("name: %s\n", s1.name);
+    printf("grade: %f\n", s1.grade);
 
     return 0;
 }
